c600f92ff77de317cefcb110fa1c4156ef9962d0
Mikael Vik <mikael@gmail.com>
2015-11-17 09:49:34 +0100

    Added FluentAssertions.

--- TestDemo/TestDemo.Tests/Controllers/ValuesControllerTests.cs
+++ TestDemo/TestDemo.Tests/Controllers/ValuesControllerTests.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using FluentAssertions;
 using NUnit.Framework;
 using TestDemo.Controllers;
 
@@ -13,6 +14,7 @@ namespace TestDemo.Tests.Controllers
             _vc = new ValuesController();
             _vc.Post("value1");
             _vc.Post("value2");
+            _vc.Post("value3");
         }
 
         private const string NewValue = "hei";
@@ -24,8 +26,14 @@ namespace TestDemo.Tests.Controllers
             var originalCount = _vc.Get().Count();
             _vc.Post(NewValue);
 
+            // vanilla NUnit
             Assert.That(_vc.Get(), Has.Member(NewValue));
             Assert.That(_vc.Get().Count(), Is.EqualTo(originalCount + 1));
+
+            // FluentAssertions 
+            _vc.Get()
+                .Should().Contain(NewValue)
+                .And.HaveCount(originalCount + 1);
         }
 
         [Test]
@@ -35,9 +43,16 @@ namespace TestDemo.Tests.Controllers
 
             _vc.Put(1, NewValue);
 
+            // vanilla NUnit
             Assert.That(_vc.Get(), Has.Member(NewValue));
             Assert.That(_vc.Get(), Has.No.Member(original));
             Assert.That(_vc.Get(1), Is.EqualTo(NewValue));
+
+            // FluentAssertions
+            _vc.Get()
+                .Should().Contain(NewValue)
+                .And.NotContain(original)
+                .And.HaveElementAt(1, NewValue);
         }
 
         [Test]
@@ -48,8 +63,14 @@ namespace TestDemo.Tests.Controllers
 
             _vc.Delete(0);
 
+            // vanilla NUnit
             Assert.That(_vc.Get().Count(), Is.EqualTo(originalSize - 1));
             Assert.That(_vc.Get(), Has.No.Member(value));
+
+            // FluentAssertions
+            _vc.Get()
+                .Should().HaveCount(originalSize - 1)
+                .And.NotContain(value);
         }
     }
 }
