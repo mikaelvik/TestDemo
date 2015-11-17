1efd9ede272f9f29091ba197704e7a0e72211c23
Mikael Vik <mikael@gmail.com>
2015-11-17 09:43:42 +0100

    Testing with vanilla NUnit.

--- TestDemo/TestDemo.Tests/Controllers/ValuesControllerTests.cs
+++ TestDemo/TestDemo.Tests/Controllers/ValuesControllerTests.cs
@@ -1,14 +1,55 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using NUnit.Framework;
+using TestDemo.Controllers;
 
 namespace TestDemo.Tests.Controllers
 {
-    [TestClass]
+    [TestFixture]
     public class ValuesControllerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [SetUp]
+        public void Init()
         {
-            Assert.AreEqual(1, 1);
+            _vc = new ValuesController();
+            _vc.Post("value1");
+            _vc.Post("value2");
+        }
+
+        private const string NewValue = "hei";
+        private ValuesController _vc;
+
+        [Test]
+        public void Posted_Value_ShouldBeFound_OnGet()
+        {
+            var originalCount = _vc.Get().Count();
+            _vc.Post(NewValue);
+
+            Assert.That(_vc.Get(), Has.Member(NewValue));
+            Assert.That(_vc.Get().Count(), Is.EqualTo(originalCount + 1));
+        }
+
+        [Test]
+        public void Put_ShouldChange_Value()
+        {
+            var original = _vc.Get(1);
+
+            _vc.Put(1, NewValue);
+
+            Assert.That(_vc.Get(), Has.Member(NewValue));
+            Assert.That(_vc.Get(), Has.No.Member(original));
+            Assert.That(_vc.Get(1), Is.EqualTo(NewValue));
+        }
+
+        [Test]
+        public void Delete_ShouldRemove_Value()
+        {
+            var originalSize = _vc.Get().Count();
+            var value = _vc.Get(0);
+
+            _vc.Delete(0);
+
+            Assert.That(_vc.Get().Count(), Is.EqualTo(originalSize - 1));
+            Assert.That(_vc.Get(), Has.No.Member(value));
         }
     }
 }
--- TestDemo/TestDemo/Controllers/ValuesController.cs
+++ TestDemo/TestDemo/Controllers/ValuesController.cs
@@ -1,39 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
 namespace TestDemo.Controllers
 {
     public class ValuesController : ApiController
     {
+        private readonly IList<string> _values = new List<string>();
+
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] {"value1", "value2"};
+            return _values;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return _values[id];
         }
 
         // POST api/values
         public void Post([FromBody] string value)
         {
+            _values.Add(value);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody] string value)
         {
+            _values[id] = value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            _values.RemoveAt(id);
         }
     }
 }
