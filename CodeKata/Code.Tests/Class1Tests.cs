ec50fedce7486464a0167a9a7149c9843644a9a3
Mikael Vik <mikael@gmail.com>
2015-11-26 10:50:29 +0100

    Added boilerplate code for CodeKata.

---
+++ CodeKata/Code.Tests/Class1Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests
+{
+    [TestFixture]
+    public class Class1Tests
+    {
+        [SetUp]
+        public void Init()
+        {
+            _class1 = new Class1();
+        }
+
+        private Class1 _class1;
+
+        [Test]
+        public void Empty_Test()
+        {
+            Assert.That(_class1, Is.Not.Null);
+        }
+    }
+}
