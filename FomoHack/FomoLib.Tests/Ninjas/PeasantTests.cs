2db0b8204567cee51099f52e94f0d32335921d2f
Mikael Vik <mikael@gmail.com>
2015-11-27 11:21:46 +0100

    Added Peasant.

---
+++ FomoHack/FomoLib.Tests/Ninjas/PeasantTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using FomoLib.Ninjas;
+using NUnit.Framework;
+
+namespace FomoLib.Tests.Ninjas
+{
+    [TestFixture]
+    public class PeasantTests
+    {
+        [Test]
+        public void Peasant_Should_Be_Damaged()
+        {
+            var peasant = new Peasant(10);
+            peasant.Defend(5).Should().Be(true);
+            peasant.Defend(3).Should().Be(true);
+            peasant.Defend(3).Should().Be(false);
+        }
+    }
+}
---
+++ FomoHack/FomoLib/Ninjas/Peasant.cs
@@ -0,0 +1,18 @@
+﻿namespace FomoLib.Ninjas
+{
+    public class Peasant : ITarget
+    {
+        private int _lifeForce;
+
+        public Peasant(int lifeForce)
+        {
+            _lifeForce = lifeForce;
+        }
+
+        public bool Defend(int damage)
+        {
+            _lifeForce -= damage;
+            return _lifeForce > 0;
+        }
+    }
+}
